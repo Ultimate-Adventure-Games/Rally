b34af3fb5d5a31b41bbc4cba270e9bf28c5a27b6
Christopher Docuyanan <christophejd@gmail.com>
2021-02-09 15:09:45 -0800

    Initial commit of react-google-maps integration.
    Co-authored-by: Vlad Munteanu <munteanuv@gmail.com>

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import './styles/styles.css'
 
 class App extends Component {
   constructor(props) {
     super(props);
   }
-  
-  
+
+
   render() {
     return (
-      <div className="app"></div>
+      <>
+        {/* <div className="app"></div> */}
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </>
     )
   }
 }
