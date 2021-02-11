c48073ddac457a71d3b16923942f4f54f9a2184d
Will <whbladon@gmail.com>
2021-02-10 20:54:16 -0800

    img upload

--- src/server/server.js
+++ src/server/server.js
@@ -13,6 +13,19 @@ app.use(bodyParser.json())
 
 app.use("/api", apiRouter);
 
+//UPLOAD IMAGES
+app.use(formData.parse())
+
+app.post('/image-upload', (req, res) => {
+
+  const values = Object.values(req.files)
+  const promises = values.map(image => cloudinary.uploader.upload(image.path))
+  
+  Promise
+    .all(promises)
+    .then(results => res.json(results))
+})
+//UPLOAD IMAGES
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`)
