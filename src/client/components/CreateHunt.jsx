e18bae2d46a51dcc7e9fe6e9336bfc55034961d5
Ali <marchofthepenguins1@gmail.com>
2021-02-10 16:21:50 -0800

    createhunt form connected to backend

--- src/client/components/CreateHunt.jsx
+++ src/client/components/CreateHunt.jsx
@@ -1,6 +1,7 @@
 import React, { useState, Fragment } from "react";
 import { Link } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.css";
+import axios from 'axios';
 
 const CreateHunt = () => {
   const [inputFields, setInputFields] = useState([
@@ -36,7 +37,27 @@ const CreateHunt = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("inputFields", inputFields);
+    // console.log("inputFields", inputFields);
+
+    const data = {
+      //const params = [req.body.hunt_name, req.body.hunt_des, req.body.hunt_votes, req.body.hunt_banner, req.body.hunt_lat, req.body.hunt_long, req.body.user_id];
+      hunt_name: inputFields[0]["eventName"],
+      hunt_des: inputFields[0]["eventDetail"],
+      hunt_lat: inputFields[0]["eventLat"],
+      hunt_lon: inputFields[0]["eventLon"],
+    }
+
+    console.log(data);
+
+    axios.post('http://localhost:3000/api/hunts/createHunt', data)
+    .then(res => {
+      if (res.status === 200) {
+        alert("Hunt successfully created. Return to Hunts List Page!")
+        props.history.push('/')
+      } else {
+        alert("Error creating account");
+      }
+    })
   };
 
   return (
