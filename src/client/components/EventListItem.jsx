f5b797909fbdf2affc3e758d254ef9230ac71a07
Christopher Docuyanan <christophejd@gmail.com>
2021-02-11 11:59:16 -0800

    Bug fix for page not loading

--- src/client/components/EventListItem.jsx
+++ src/client/components/EventListItem.jsx
@@ -6,27 +6,6 @@ import { AppContext } from './ContextProvider';
 const EventListItem = ({ title, description, onSelect, uploadPhotoHandler, id }) => {
     const { user } = useContext(AppContext);
 
-    
-const uploadImage = e => {
-    const files = Array.from(e.target.files)
-  
-    const formData = new FormData()
-  
-    files.forEach((file, i) => {
-      formData.append(i, file)
-    })
-  
-    fetch(`http://localhost:3000/api/photos/image-upload/` + id + '/' + user.user_id, {
-      method: 'POST',
-      body: formData
-    })
-    .then(res => res.json())
-    .then(images => {
-        console.log(images)
-      //refresh state
-    })
-  }
-
     return (
         <div className="list-item-container">
             <div className="listItem">
@@ -42,7 +21,7 @@ const uploadImage = e => {
                     <div className="navigate">
                         <button onClick={onSelect}>View</button>
                         {/* <input type='file' id='single' onChange={uploadImage} />  */}
-                        <ImageUpload uploadPhotoHandler={uploadPhotoWithId} title="&#10003;"/>
+                        <ImageUpload uploadPhotoHandler={uploadPhotoHandler} title="&#10003;"/>
                     </div>
                 </div>
             </div>
